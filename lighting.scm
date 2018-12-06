da13e72beff7101145b75d6581c935401c6b5e30
Zack Buhman <zack@buhman.org>
2018-12-05 18:05:07 -0800

    lighting: wip

--- lighting.scm
+++ lighting.scm
@@ -115,7 +115,9 @@
     (match es
       ((edge . rest)
        (if (edges-intersect? ray edge)
-         (cons (car ray) (ray-intersect-edge ray edge))
+         (begin
+           (equal? (cdr ray) (ray-intersect-edge ray edge)))
+         ;;(cons (car ray) (ray-intersect-edge ray edge)))
          (loop rest)))
       (() #f))))
 
@@ -123,14 +125,89 @@
 (define edge2 '((100 . 200) . (200 . 100)))
 (define edge3 '((400 . 400) . (550 . 500)))
 
+(define (ray-pair->quad r1 r2)
+  (let ((a (car r1))
+        (b (cdr r1))
+        (c (cdr r2)))
+    (trapezoid-decompose (points->edges (list a b c)))))
+
+(define (rays->quads rays)
+  (let loop ((rs rays)
+             (lr #f))
+    (match rs
+      ((ray . rest)
+       (if lr
+         (append (ray-pair->quad lr ray)
+                 (loop rest ray))
+         (loop rest ray)))
+      (() '()))))
+
 (define (render-lighting! renderer grip)
   (let* ((points-list (obstruction-points grip))
          (center *mouse*)
          (edges (obstruction-edges center points-list))
          (rays (points->rays center points-list)))
 
-    (let ((r2 (map (lambda (r) (ray-cast r edges)) rays)))
-      (set! (sdl2:render-draw-color renderer) (multiply-alpha +yellow+ 64))
-      (render-draw-edges! renderer rays)
-      (set! (sdl2:render-draw-color renderer) (multiply-alpha +green+ 64))
-      (render-draw-edges! renderer r2))))
+    (let* (;;(r2 (map (lambda (r) (ray-cast r edges)) rays))
+           (r2 (filter (lambda (r) (ray-cast r edges)) rays))
+           (r3 (sort r2 (angle-compare <))))
+      (set! (sdl2:render-draw-color renderer) (multiply-alpha +white+ 64))
+      ;;(render-draw-edges! renderer rays)
+      (set! *rays* r3)
+
+      (let ((quads (rays->quads r3)))
+        (map
+         (lambda (t ix)
+           (let* ((c (hue->rgb (/ ix (length quads))))
+                  (color (C (car c) (cadr c) (caddr c) 128)))
+             (set! (sdl2:render-draw-color renderer) color)
+             (render-draw-trapezoid! renderer t)))
+         quads (iota (length quads) 0)))
+
+      (render-draw-rays-debug! renderer r3)
+
+      (map
+       (lambda (f)
+         (let* ((c (hue->rgb (/ f 256)))
+                (color (C (car c) (cadr c) (caddr c))))
+           (set! (sdl2:render-draw-color renderer) color)
+           (sdl2:render-draw-point! renderer (+ 100 f) 100)))
+       (iota 256 0)))))
+
+(define (hue->rgb h)
+  (let ((r (- (abs (- 3 (* h 6))) 1))
+        (g (- 2 (abs (- 2 (* h 6)))))
+        (b (- 2 (abs (- 4 (* h 6))))))
+    (map
+     (lambda (c)
+       (exact/round (* 254 (/ (+ 2 c) 4))))
+     (list r g b))))
+
+(define (render-draw-rays-debug! renderer rays)
+  (map
+   (lambda (ray ix)
+     (let* ((c (hue->rgb (/ ix (length rays))))
+            (color (C (car c) (cadr c) (caddr c) 128)))
+       (set! (sdl2:render-draw-color renderer) color))
+     (match ray
+       (((xa . ya) . (xb . yb))
+        (sdl2:render-draw-line! renderer xa ya xb yb))))
+   rays (iota (length rays) 0)))
+
+(define (normalize angle)
+  (if (< angle 0)
+    (+ +pi+ (- +pi+ (abs angle)))
+    angle))
+
+(define (angle edge)
+  (match edge
+    (((ax . ay) . (bx . by))
+     (let ((dx (- bx ax))
+           (dy (- by ay)))
+       (atan dy dx)))))
+
+(define (angle-compare cmp)
+  (lambda (a b)
+    (let ((ta (angle a))
+          (tb (angle b)))
+      (cmp ta tb))))
